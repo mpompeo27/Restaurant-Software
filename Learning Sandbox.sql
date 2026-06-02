74976e746fc148765c1fe3c62f1ec96c86df6ccd
mpompeo27 <mpompeo27@gmail.com>
2026-06-01 18:27:18 -0600

    Delete Learning Sandbox.sql

--- Learning Sandbox.sql
+++
@@ -1,4 +0,0 @@
--- Learning sandbox for designing the database for the Restaurant Software web app project
-
--- Add a 'server' column to the 'tables' table
-ALTER TABLE tables ADD COLUMN server_id INTEGER REFERENCES staff(id);
